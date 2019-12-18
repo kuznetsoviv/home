dfe27d0bbd8bc1c56b2c5d1158c41b204e4787f3
Igor <kuznetsoviv1992@yandex.ru>
2019-12-18 22:51:08 +0300

    exanples work with dates

---
+++ date-time/src/main/java/ru/kuznetsoviv/zip/Main.java
@@ -0,0 +1,19 @@
+package ru.kuznetsoviv.zip;
+
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Europe/London") , new Locale("en"));
+        System.out.println("Установка и получение времени:");
+        //calendar.set(Calendar.HOUR_OF_DAY, 0);
+        System.out.println(calendar.getTime());
+        System.out.println("Получение параметров календаря:");
+        System.out.println("------------------------------");
+        System.out.println(calendar);
+    }
+
+}
